76c8bd3d930835437c3e001697d098d0ed311f57
Neil An <nan1@myseneca.ca>
2022-09-10 20:32:10 -0400

    added index.html feature

--- generateHTML.js
+++ generateHTML.js
@@ -48,6 +48,7 @@ export function generateHTML(input)
                             })
                         }
                     })
+                    generateIndexHTML(files, true);
                 })
             }
             else
@@ -57,6 +58,7 @@ export function generateHTML(input)
                     readFile(input).then(function(result)
                     {
                         writeFile(input, result);
+                        generateIndexHTML(input, false);
                     })
                     .then(function(err)
                     {
@@ -78,25 +80,10 @@ function readFile(input)
         const line = readline.createInterface(
             {
                 input: theFile
-                // ,
-                // crlfDelay: Infinity
             }
         );
     
         //go through the file line by line
-        // line.on('line', function(theLine)
-        // {
-        //     console.log("line: " + theLine);
-        //     //check if line is not empty
-        //     if (theLine != "")
-        //     {
-        //         lineArray.concat(theLine);
-        //     }
-        //     else
-        //     {
-        //         lineArray.concat("\n");
-        //     }
-        // });
         for await (const theLine of line)
         {
             //console.log("theline" + theLine);
@@ -119,52 +106,96 @@ function writeFile(input, result)
 {
     return new Promise(function(res, rej)
     {
-        const templateFirst =
-        `
-        <!doctype html>
-        <html lang="en">
-        <head>
-            <meta charset="utf-8">
-            <title>Filename</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-        </head>
-        <body>
-`
-    
-        const templateSecond =
-        `
-        </body>
-        </html>
-        `;
-
         var content = "";
+        var lineNumber = 0;
         //create the file name
         var htmlFile = './dist/' + input.substring(0, input.length-4) + '.html';
 
-        //console.log("res" + result);
-
+        //create the content to write
         for (var theLine of result)
         {
             if (theLine != "\n")
             {
-                content += "\t\t<p>" + theLine + "</p>\n";
+                if (lineNumber == 0)
+                {
+                    content += "<p>" + theLine + "</p>\n";
+                }
+                else
+                {
+                    content += "\t<p>" + theLine + "</p>\n";
+                }
             }
             else
             {
                 content += "\n";
             }
+            lineNumber++;
         }
 
-        //console.log(content)
-        //create the complete HTML
-        var completeHTML = templateFirst + content + templateSecond;
+        const templateHTML =
+        `
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Filename</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    ${content}
+</body>
+</html>
+        `
 
         //have to write the file contents now
-        fs.writeFile(htmlFile, completeHTML, function(err)
+        fs.writeFile(htmlFile, templateHTML, function(err)
         {
             console.log(err);
         });
+
         console.log("second");
         res(htmlFile);
     })
+}
+
+function generateIndexHTML(input, isDir)
+{
+    var content = "";
+    if (isDir)
+    {
+        for (var file of input)
+        {
+            var htmlFile = input.substring(0, input.length-4) + '.html';
+            var htmlFileReadable = input.substring(0, input.length-4) + '.html';
+            content += `<a href="${htmlFile}"> ${htmlFileReadable} </a><br>`;
+            console.log(file);
+        }
+    }
+    else
+    {
+        var htmlFile = input.substring(0, input.length-4) + '.html';
+        var htmlFileReadable = input.substring(0, input.length-4) + '.html';
+        content += `<a href="${htmlFile}"> ${htmlFileReadable} </a><br>`;
+    }
+
+    const templateHTML =
+        `
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Filename</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    The files are:
+    <br>
+    ${content}
+</body>
+</html>
+        `
+        fs.writeFile('./dist/index.html', templateHTML, function(err)
+        {
+            console.log(err);
+        });
 }
