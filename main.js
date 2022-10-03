903d640ce0268769ddac7b9619823757053f5a35
alexsam29 <69481177+alexsam29@users.noreply.github.com>
2022-10-03 02:50:23 -0400

    Added ability to read from config file.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 import { generateHTML } from './generateHTML.js';
 import { program } from 'commander';
+import fs, { read } from 'fs';
 
 program
 .option('-v, --version', 'displays the tool name and version')
@@ -18,7 +19,13 @@ if (program.opts().version)
 
 if(program.opts().config)
 {
-    
+    readConfigFile(program.opts().config).then(function(configFile)
+    {
+        generateHTML(configFile.input, configFile.lang);
+    }).catch(function (rej)
+    {
+        console.log(rej)
+    })
 }
 if (program.opts().input)
 {
@@ -33,3 +40,20 @@ if (program.opts().input)
     }
 }
 
+//this function will read a JSON config file
+function readConfigFile(config)
+{
+    return new Promise(function(res, rej)
+    {
+        if(fs.existsSync(config))
+        {
+            const theFile = fs.readFileSync(config, "utf-8");
+            const JSONfile = JSON.parse(theFile);
+            res(JSONfile);    
+        }
+        else
+        {
+            rej("Error: Config file does not exist.")
+        }
+    })
+}
